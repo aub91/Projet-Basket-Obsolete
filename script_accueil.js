39a443002179e25cb88ce91c822c0314c134bc2c
aubin <aubin.guilhem@gmail.com>
2017-08-29 19:09:30 +0200

    remplacement accès aux markers
    Remplacement du fichier xml par une description JSON de la liste.
    Adaption de la création de la map suite à ce changement.

--- script_accueil.js
+++ script_accueil.js
@@ -33,28 +33,34 @@ function showLogin() {
         });
         var infoWindow = new google.maps.InfoWindow;
 		var markerTab = [];
+		
           // Change this depending on the name of your PHP or XML file
-          downloadUrl('placeData.xml', function(data) {
-            var xml = data.responseXML;
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var type = markerElem.getAttribute('type');
+            var markers = {
+				"marker": [
+					{"name": "Ferme de Paris", "address": "1 route de Pesage, 75012 Paris, France", "lat": "48.822477", "lng": "2.444399", "type": "producteur"},
+					{"name": "Supérette d'Orléans", "address": "130 Boulevard Brune, 75014 Paris, France", "lat": "48.823423", "lng": "2.323553", "type": "pointRelais"},
+					{"name": "Supérette", "address": "21 Avenue de Verdun, 92170 Vanves, France", "lat": "48.820826", "lng": "2.294402", "type": "pointRelais"},
+					{"name": "Ferme de Gally", "address": "33 Rue de Chatou, 78500 Sartrouville, France", "lat": "48.941605", "lng": "2.189682", "type": "producteur"},
+					{"name": "Ferme pédagogique de la Butte Pinson", "address": "16 Rue Suzanne Valadon, 95360 Montmagny", "lat": "48.967287", "lng": "2.353129", "type": "producteur"}			
+				]
+			};
+			
+            for (var i = 0; i<markers.marker.length; i++) {
+              var name = markers.marker[i].name;
+              var address = markers.marker[i].address;
+              var type = markers.marker[i].type;
               var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
-
+                  parseFloat(markers.marker[i].lat),
+                  parseFloat(markers.marker[i].lng));
               var infowincontent = document.createElement('div');
               var strong = document.createElement('strong');
-              strong.textContent = name
+              strong.textContent = name;
               infowincontent.appendChild(strong);
               infowincontent.appendChild(document.createElement('br'));
-
               var text = document.createElement('text');
-              text.textContent = address
+              text.textContent = address;
               infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
+              var icon = customLabel[markers.marker[i].type] || {};
               var marker = new google.maps.Marker({
                 map: map,
                 position: point,
@@ -64,31 +70,13 @@ function showLogin() {
                 infoWindow.setContent(infowincontent);
                 infoWindow.open(map, marker);
               });
+			  
 			  markerTab.push(marker);
-            });
+            }
+			
 			var markerCluster = new MarkerClusterer(map, markerTab,
             {imagePath: 'markerClusterer/m'});
-          });
-		  
-		  
-		  // Add a marker clusterer to manage the markers.
-        
-        }
-
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-      }
+			
+          };
 
       function doNothing() {}
